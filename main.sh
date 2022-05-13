e2a77859f2a15250d3f0c812ad059edf473f96c7
DanielLin97 <31728535+DanielLin97@users.noreply.github.com>
2022-05-13 12:07:55 +0800

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#pip install -U torch==1.4.0 numpy==1.18.1
+#pip install -r requirements.txt
+#Generate graph data and store in /data/Weibograph
+python ./Process/getWeibograph.py Weibo
+#Reproduce the experimental results.
+CUDA_VISIBLE_DEVICES=0 python ./model/Weibo/ACLR_Weibo.py Weibo 1
